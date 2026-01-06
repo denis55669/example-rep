1aba3470a6eb653fa38a46118bf9db71b67e4f6b
denis55669 <100089446+denis55669@users.noreply.github.com>
2026-01-06 06:49:30 +0200

    index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,26 @@
-import { EventsSDK } from "github.com/octarine-public/wrapper/index"
+import { EventsSDK, EntitySDK, ItemSDK, Menu, ImageData } from "github.com/octarine-public/wrapper/index"
 
-console.log("Hello world!")
-EventsSDK.on("GameStarted", () => {
-	console.log("Hello world!")
-})
+const MyScripts = Menu.AddEntryDeep(["Custom Scripts", "Денис"])
+const ArmletNode = MyScripts.AddNode("Абуз Армлета", ImageData.Icons.item_armlet)
+
+const IsEnabled = ArmletNode.AddToggle("Увімкнути", true)
+const HpThreshold = ArmletNode.AddSlider("Поріг Здоров'я", 300, 50, 600, 10)
+const CancelAttack = ArmletNode.AddToggle("Відміна атаки", true)
+
+EventsSDK.on("GameTick", () => {
+    if (!IsEnabled.Value) return;
+
+    const me = EntitySDK.getLocalPlayer();
+    if (!me || !me.isAlive()) return;
+
+    const armlet = ItemSDK.getItemByName(me, "item_armlet");
+    if (!armlet) return;
+
+    const health = me.getHealth();
+    const isToggled = ItemSDK.isToggled(armlet);
+
+    if (health <= HpThreshold.Value && isToggled) {
+        ItemSDK.useItem(armlet);
+        ItemSDK.useItem(armlet);
+    }
+});
