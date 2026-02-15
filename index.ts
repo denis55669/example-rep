a2e67572afba991ce2bfa2131d12e93e9dd71ce6
denis55669 <100089446+denis55669@users.noreply.github.com>
2026-02-15 15:47:40 +0200

    index.ts

--- index.ts
+++ index.ts
@@ -2,23 +2,16 @@ import {
     EventsSDK,
     LocalPlayer,
     Menu,
-    Vector3,
-    Render
+    Vector3
 } from "github.com/octarine-public/wrapper/index"
 
-// --- МЕНЮ ---
-const Main = Menu.AddEntry("Denis_Scripts_V14");
+// --- МАКСИМАЛЬНО ПРОСТЕ МЕНЮ (Без іконок і списків) ---
+const Main = Menu.AddEntry("Denis_V15");
+const FeedRadiant = Main.AddToggle("1. ФІД ЗА RADIANT", false); // Бігти вгору (Dire)
+const FeedDire = Main.AddToggle("2. ФІД ЗА DIRE", false);       // Бігти вниз (Radiant)
+const TechiesOn = Main.AddToggle("3. ТЕЧІС (На кнопку F)", false);
 
-// Фідер
-const EnableFeed = Main.AddToggle("1. УВІМКНУТИ ФІД", false);
-const FeedSide = Main.AddList("2. Твій бок (Команда)", ["Я за RADIANT", "Я за DIRE"], 1);
-
-// Течіс
-const EnableTechies = Main.AddToggle("3. ТЕЧІС (АВТО-МІНЕР)", false);
-const TechKey = Main.AddKeybind("4. Малювання (Затиснути)", 0x46); // Кнопка F
-
-let lastFTime = 0;
-let lastTTime = 0;
+let lastF = 0;
 let tPoints: Vector3[] = [];
 
 EventsSDK.on("PostDataUpdate", () => {
@@ -27,67 +20,44 @@ EventsSDK.on("PostDataUpdate", () => {
     const now = Date.now();
 
     // --- ЛОГІКА ФІДУ ---
-    if (EnableFeed.value && now - lastFTime > 4000) {
-        lastFTime = now;
-        
-        let targetPos: Vector3;
-        if (FeedSide.value === 1) { 
-            // ТИ ЗА DIRE (ТЬМА) -> Біжиш на фонтан Світлих
-            targetPos = new Vector3(-7200, -6600, 384);
-        } else {
-            // ТИ ЗА RADIANT (СВІТЛО) -> Біжиш на фонтан Тьми
-            targetPos = new Vector3(7200, 6500, 384);
+    if (now - lastF > 5000) {
+        if (FeedRadiant.value) {
+            // Ти за світло -> біжиш до темних (вгору-вправо)
+            // @ts-ignore
+            Me.MoveTo(new Vector3(7200, 6500, 384));
+            lastF = now;
+        } else if (FeedDire.value) {
+            // Ти за тьму -> біжиш до світлих (вниз-вліво)
+            // @ts-ignore
+            Me.MoveTo(new Vector3(-7200, -6600, 384));
+            lastF = now;
         }
-        
-        // Додаємо трохи рандому, щоб не палитися
-        const finalTarget = new Vector3(
-            targetPos.x + (Math.random() * 400 - 200),
-            targetPos.y + (Math.random() * 400 - 200),
-            targetPos.z
-        );
+    }
 
+    // --- ЛОГІКА ТЕЧІСА (БЕЗ МАЛЮВАННЯ, ТІЛЬКИ КНОПКА F) ---
+    if (TechiesOn.value && Me.UnitName === "npc_dota_hero_techies") {
+        // Якщо затиснута клавіша F (0x46)
         // @ts-ignore
-        Me.MoveTo(finalTarget);
-    }
-
-    // --- ЛОГІКА ТЕЧІСА ---
-    if (EnableTechies.value && Me.UnitName === "npc_dota_hero_techies") {
-        
-        // Малювання точок
-        if (TechKey.value) {
+        if (EventsSDK.IsKeyDown(0x46)) {
             // @ts-ignore
-            const mPos = EventsSDK.GetCursorPosWorld(); 
-            if (mPos) {
-                if (tPoints.length === 0 || mPos.Distance(tPoints[tPoints.length - 1]) > 300) {
-                    tPoints.push(mPos);
-                }
+            const m = EventsSDK.GetMousePosWorld();
+            if (m && (tPoints.length === 0 || m.Distance(tPoints[tPoints.length - 1]) > 200)) {
+                tPoints.push(m);
             }
         }
 
-        // Встановлення мін
-        if (!TechKey.value && tPoints.length > 0 && now - lastTTime > 1100) {
+        // Авто-каст мін
+        if (tPoints.length > 0 && now - lastF > 1000) {
             // @ts-ignore
-            const mine = Me.GetAbility("techies_land_mines");
-            if (mine && mine.CanBeCasted()) {
+            const ability = Me.GetAbility("techies_land_mines");
+            if (ability && ability.CanBeCasted()) {
                 // @ts-ignore
-                Me.CastAbilityPosition(mine, tPoints[0]);
+                Me.CastAbilityPosition(ability, tPoints[0]);
                 tPoints.shift();
-                lastTTime = now;
+                lastF = now;
             }
         }
     }
 });
 
-// Візуалізація точок (щоб ти бачив, що малювання працює!)
-EventsSDK.on("OnDraw", () => {
-    if (EnableTechies.value && tPoints.length > 0) {
-        tPoints.forEach(p => {
-            const sPos = Render.WorldToScreen(p);
-            if (sPos) {
-                Render.DrawCircle(sPos, 6, [0, 255, 0, 255], 2); // Зелені точки
-            }
-        });
-    }
-});
-
-console.log("Denis V14 Loaded: Feed & Techies drawing fixed!");
+console.log("Denis V15 Ready!");
