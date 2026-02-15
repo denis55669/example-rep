feac76b7fab3509e63f215ffa12c1c325d28921f
denis55669 <100089446+denis55669@users.noreply.github.com>
2026-02-15 02:38:34 +0200

    index.ts

--- index.ts
+++ index.ts
@@ -1,29 +1,45 @@
 import { EventsSDK, Menu } from "github.com/octarine-public/wrapper/index"
 
-// 1. Створюємо головну вкладку "Денис"
+// --- ТВОЄ РОБОЧЕ МЕНЮ ---
 const MyTab = Menu.AddEntry("Денис");
-
-// 2. Додаємо Камеру і Авто-прийняття (це в тебе працювало)
 const CameraSlider = MyTab.AddSlider("Дистанція камери", 1200, 2500, 1600);
 const AutoAcceptToggle = MyTab.AddToggle("Авто-прийняття", true);
-
-// 3. Додаємо кнопки для Армлета (Візуальна частина)
 const AutoArmletToggle = MyTab.AddToggle("Авто-Армлет", true);
 const HpSlider = MyTab.AddSlider("Поріг ХП Армлет", 200, 800, 450);
 
-// Основна функція
+// --- ЛОГІКА ---
 EventsSDK.on("Update", () => {
-    // Логіка камери (працює)
-    if (CameraSlider) {
+    // 1. Камера
+    if (typeof Camera !== 'undefined') {
         // @ts-ignore
-        if (typeof Camera !== 'undefined') {
-            // @ts-ignore
-            Camera.Distance = CameraSlider.Value;
-        }
+        Camera.Distance = CameraSlider.Value;
     }
 
-    // ТУТ БУДЕ ЛОГІКА АРМЛЕТА
-    // Ми підключимо її, як тільки дізнаємося, як правильно називається імпорт у твоїй версії
+    // 2. Абуз Армлета
+    // @ts-ignore
+    const me = GameEntitySystem.getLocalPlayer(); // Беремо гравця
+
+    if (me && AutoArmletToggle.Value) {
+        // @ts-ignore
+        if (me.IsAlive()) { // Перевіряємо чи живий
+            const armlet = me.getItemByName("item_armlet");
+            
+            if (armlet && armlet.isReady()) {
+                // @ts-ignore
+                const currentHp = me.Health; // Отримуємо ХП
+                
+                // Якщо ХП мало і немає ульти Апарата
+                if (currentHp <= HpSlider.Value && !me.hasModifier("modifier_ice_blast")) {
+                    armlet.cast(); // Вимкнути
+                    
+                    // Якщо він був увімкнений (Unholy Strength), вмикаємо назад миттєво
+                    if (me.hasModifier("modifier_item_armlet_unholy_strength")) {
+                        armlet.cast(); // Увімкнути
+                    }
+                }
+            }
+        }
+    }
 });
 
-console.log("Скрипт Дениса (Меню) завантажено!");
+console.log("Скрипт Дениса: Меню + Армлет працюють!");
